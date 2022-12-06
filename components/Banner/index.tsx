5fd3f772b85ee6f05b5ff916415d10e596496a5a
masudrana2779 <masudrana2779@gmail.com>
2022-12-06 16:55:14 +0600

    banner slider dots design done

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -4,7 +4,7 @@ import { BannerWrap } from "./banner.styled";
 
 const BannerComponent = () => {
   var settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 2000,
     slidesToShow: 1,
