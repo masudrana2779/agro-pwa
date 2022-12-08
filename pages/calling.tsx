f9aef895c9712c51087f643647bf0a7d9b371e4f
masudrana2779 <masudrana2779@gmail.com>
2022-12-08 23:44:50 +0600

    try to done calling design

---
+++ pages/calling.tsx
@@ -0,0 +1,12 @@
+import CallingComponent from "../components/common/Calling";
+import Layout from "../components/Layouts";
+
+export default function Calling() {
+  return (
+    <>
+      <Layout>
+        <CallingComponent />
+      </Layout>
+    </>
+  );
+}
